264a21ab4651754eb871afbc103804fe39174544
Christopher Marshall <thoughtpalettechris@gmail.com>
2015-09-17 14:18:03 -0500

    Initial state loading

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import Firebase from 'firebase';
+import Router from 'react-router';
+import axios from 'axios';
 
-class Main extends React.Component{
+class Main extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            users: ["chris"]
+        }
+    }
+    // componentDidMount() {
+
+        // this.ref = new Firebase('https://torrid-torch-7417.firebaseio.com');
+
+        // var childRef = this.ref.child('users');
+
+        // this.bindAsArray(childRef, 'users');
+
+        // this.ref.on("value", function ( snapshot )
+        // {
+        //     this.setState({
+        //         users: snapshot.val()
+        //     });
+        // }.bind(this));
+    // }
     render() {
         return (
-            <div> Hi, this is a test page </div>
+            <div> Hi, this is a test page {this.state.users}</div>
         )
     }
 };
